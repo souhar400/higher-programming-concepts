3b5db78bfdd0060dfa9affe8d5917b383bbe21fa
Lukas <lw874718@fh-muenster.de>
2021-11-12 18:36:30 +0100

    engine

---
+++ mxr.engine/src/main/java/de/lab4inf/mxr/engine/Key.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package de.lab4inf.mxr.engine;
+
+import java.util.Objects;
+
+import de.lab4inf.mxr.core.Fact;
+import de.lab4inf.mxr.core.NonNull;
+
+/**
+ * @author lukas
+ *
+ */
+class Key<Problem, facts extends Fact> {
+	private Problem p;
+	private Class<?> f;
+	private Class<?> sig[];
+ 
+	
+	public Key(Problem p, facts f) {
+		this.p = Objects.requireNonNull(p);
+		this.f = f.getClass();
+		this.sig = f.signature();
+	}
+	@Override
+	public int hashCode() {
+		int sigHash = 0;
+		for(Class<?> clazz: this.sig) {
+			sigHash += clazz.hashCode();
+		}
+		return this.p.hashCode()^this.f.hashCode()^sigHash;
+		}
+	@Override
+	public boolean equals(Object o) {
+		if (null == o)
+			return false;
+		if (this == o)
+			return true;
+		if (o.getClass() == this.getClass()) {
+			@NonNull
+			Key<?, ?> that = Key.class.cast(o);
+			return this.p.equals(that.p) 
+					&& this.f.equals(that.f)
+					&& this.sigEquals(that.sig);
+		}
+		return false;
+	}
+	protected boolean sigEquals(Class<?> signature[]) {
+		if(signature.length == this.sig.length) {
+			int i = 0;
+			boolean rv= true;
+			for(Class<?> clazz: this.sig) {
+				rv = rv && clazz.equals(signature[i++]);
+				return rv;
+		}
+		}
+		return false;
+	}
+}
---
+++ mxr.engine/src/test/java/de/lab4inf/mxr/engine/MXREngineTest.java
@@ -0,0 +1,19 @@
+package de.lab4inf.mxr.engine;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.lab4inf.mxr.core.Fact2D;
+import de.lab4inf.mxr.core.FactVA;
+import de.lab4inf.mxr.core.Mops;
+ 
+class MXREngineTester{
+	
+	
+}
