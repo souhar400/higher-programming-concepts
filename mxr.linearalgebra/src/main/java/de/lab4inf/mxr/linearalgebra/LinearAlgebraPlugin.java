77702df5c5eefe9d5459d1f58204f5129b4be159
souhail <sa247031@fh-muenster.de>
2021-11-28 22:28:35 +0100

    mop im Plugin added + getestet

--- mxr.linearalgebra/src/main/java/de/lab4inf/mxr/linearalgebra/LinearAlgebraPlugin.java
+++ mxr.linearalgebra/src/main/java/de/lab4inf/mxr/linearalgebra/LinearAlgebraPlugin.java
@@ -1,6 +1,8 @@
 package de.lab4inf.mxr.linearalgebra;
 
 import de.lab4inf.mxr.core.Fact2D;
+import de.lab4inf.mxr.core.Fact3D;
+
 import de.lab4inf.mxr.core.MXR;
 import de.lab4inf.mxr.linearalgebra.tools.MathProblem;
 
@@ -16,7 +18,8 @@ public class LinearAlgebraPlugin implements MXR.Plugin {
 		container.register(MathProblem.MULT, Fact2D.fact(vector, vector), new VecMult());
 		container.register(MathProblem.ADD, Fact2D.fact(matrix, matrix), new MatrxAdder());
 		container.register(MathProblem.MULT, Fact2D.fact(matrix, matrix), new MatrxMult());
-		container.register(MathProblem.MULT, Fact2D.fact(matrix, vector), new MatrxVecMult());
+		container.register(MathProblem.MULT, Fact2D.fact(matrix, vector), new MatrxVecMult());		
+		container.register(MathProblem.OPTMULT, Fact3D.fact(matrix, matrix, false), new MatrxMultOptimized());
 	}
 
 }
