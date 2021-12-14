51385541e7affbc9f4d6776b65116d3138ca1383
souhail <sa247031@fh-muenster.de>
2021-12-14 14:33:20 +0100

    test verfeinert

--- mxr.math/src/mxr_math/cpp/Differentiator.cpp
+++ mxr.math/src/mxr_math/cpp/Differentiator.cpp
@@ -14,8 +14,8 @@ double differentiate(Function &f, double x, double eps)
 	int n;
 	double actualAblt, lastAblt, oldDelta, delta;
 
-	double h = 1e-5;
-	//double h= pow(DBL_EPSILON*2.0, (1.0/3.0));
+	//double h = 1e-5;
+	double h= pow(DBL_EPSILON*2.0, (1.0/3.0));
 
 	actualAblt = (f(x+h)-f(x-h))/(2.0*h);
 	delta = abs(actualAblt);
@@ -26,6 +26,10 @@ double differentiate(Function &f, double x, double eps)
 		h = h/2.0;
 		actualAblt = (f(x+h)-f(x-h))/(2.0*h);
 		delta=abs(lastAblt-actualAblt);
-	} while(delta>eps && delta<oldDelta);
-				return actualAblt;
+ 	//} while(delta>eps && delta<oldDelta);
+	} while(delta>eps);
+		return actualAblt;
 }
+
+
+
